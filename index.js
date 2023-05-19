cff9d9e9d728b5a961126fb5d679bb94393933d9
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-05-19 14:26:32 -0600

    added to allowedOrgins

--- index.js
+++ index.js
@@ -9,23 +9,22 @@ const Models = require('./models.js');
 
 const cors = require('cors');
 app.use(cors());
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'https://austinmovieapp.herokuapp.com'];
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Authentication (auth.js is handling login endpoint and generating JWT tokens)
 let auth = require('./auth.js')(app);
 const passport = require('passport');
-
 require('./passport.js');
 
 // Input Validation
