116ed6eb89cf63c93d346cde190905b75ee175d1
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-05-24 15:40:37 -0600

    Added authentication to /movies response

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/documentation', (req, res) => {
 
 
 //GET all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
