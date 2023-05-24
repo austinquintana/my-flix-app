3dd70ff9ede003c7d71414ce0b99770948bc3563
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-05-24 15:24:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/documentation', (req, res) => {
 
 
 //GET all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
@@ -179,7 +179,7 @@ app.post('/users',
 app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.find()
     .then((users) => {
-      res.status(201).json(users);
+      res.status(200).json(users);
     })
     .catch((err) => {
       console.error(err);
